25d7a01e6744238dd9171760c96b75cce20a38d0
VMistry <virenmistry@sky.com>
2018-08-11 09:36:01 +0100

    added comments

--- js/index.js
+++ js/index.js
@@ -1,28 +1,20 @@
-//found the
+//find the squares by its class name and store it in divsquare.
 var divSquare = $(".square");
+//This will hold the player number. This will update once the player had made thier move.
 var turnCount = 1;
+var positionArray = [0,0,0,0,0,0,0,0,0];
 
-// divSquare[0].addEventListener("click", function(event){
-//   if(turnCount == 1){
-//     event.stopPropagation();
-//     divInnerSquare[0].className = "innerSquare circle";
-//     turnCount = 2;
-//   }
-//   else{
-//     event.stopPropagation();
-//     divInnerSquare[0].className = "innerSquare circle2";
-//     turnCount = 1;
-//   }
-// })
-
-
+//This click event will find whihc div has been clicked and edit the inner divs to a O or an X.
 divSquare.on('click', function(){
+  //This targets the div inside of the selected div.
   var divInnerSquare = $('.innerSquare', this);
+  //If it is player ones turn, do this.
   if(turnCount == 1){
     event.stopPropagation();
     divInnerSquare.addClass('circle');
     turnCount = 2;
   }
+  //If it is player twos turn, do this.
   else{
     event.stopPropagation();
     divInnerSquare.addClass('circle2');
